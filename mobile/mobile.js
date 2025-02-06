c98469b1b5248527d33f21a1128299a9b600a254
mrXim <duc.buingoc89@gmail.com>
2025-02-06 09:52:44 +0700

    Update mobile.js

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -1,2 +1,70 @@
-console.log('mobile.js');
-alert('mobile.js 2');
+$(document).ready(function () {
+	var startAll = new Date();
+	var _tables = document.querySelectorAll("table");
+	_tables.forEach(table => processTable(table));
+
+	setTimeout(() => {
+		processImage();
+	}, 3000);
+
+	var _processed = [];
+	function processTable(table) {
+		try {
+			var start = new Date();
+			rasterizeHTML
+				.drawHTML(table.outerHTML)
+				.then(renderResult => {
+					const img = new Image();
+					img.src = renderResult.image.src;
+					table.replaceWith(img);
+					console.log('table image', (new Date() - start), (new Date() - startAll));
+				})
+				.finally(() => {
+					processedTable(table);
+				});
+		} catch (ex) {
+			console.error(ex);
+			processedTable(table);
+		}
+	}
+
+	function processedTable(table) {
+		_processed.push(table);
+		if (_processed.length === _tables.length) {
+			processImage();
+		}
+	}
+
+	var processedImage = false;
+	function processImage() {
+		console.log('processImage', processedImage);
+		try {
+			if (processedImage) {
+				return;
+			}
+			processedImage = true;
+
+			//-- lazyload image
+			$("img").lazyload({
+				threshold: 200,
+				skip_invisible: true,
+				placeholder: "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw",
+				load: function () {
+					$(window).trigger("scroll")
+				}
+			});
+
+			//-- event click image
+			$("img").click(function () {
+				webkit.messageHandlers.callbackHandler.postMessage($(this).attr("data-original"))
+			});
+
+			//-- complet load image
+			$("img").on("load", function () {
+				webkit.messageHandlers.callbackHandler.postMessage(document.documentElement.getBoundingClientRect().height)
+			});
+		} catch (ex) {
+			console.error(ex);
+		}
+	}
+});
